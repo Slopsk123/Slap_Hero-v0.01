8f195d8ab1b68394b41addb2da4dd8023f6a596c
Vítězslav Mrkos <vitamrkos@gmail.com>
2025-02-10 14:30:51 +0100

    asi to nebude fungovat

--- main.js
+++ main.js
@@ -45,16 +45,46 @@ function updateCanvas(){
 
     canvas.clear();
     canvas.add(text);
-    var arrowType =(!isInInterval) ? 'circle.png' : (expKey == "w") ? 'arrowUp.png' : (expKey == "d") ? 'arrowRight.png' : (expKey == "s") ? 'arrowDown.png' : 'arrowLeft.png';
-    fabric.Image.fromURL('https://raw.githubusercontent.com/Slopsk123/Slap_Hero-v0.01/refs/heads/master/img/git ' + arrowType, function(arrow) {
+    fabric.Image.fromURL('https://raw.githubusercontent.com/Slopsk123/Slap_Hero-v0.01/refs/heads/master/img/' + expKey + "Arrow.png", function(arrow) {
+        arrow.set({ 
+            left: 200,
+            top: 100,
+            selectable: false
+        });
+        arrow.scaleToWidth(200);
+        canvas.add(arrow);
+       });
+
+       fabric.Image.fromURL('https://raw.githubusercontent.com/Slopsk123/Slap_Hero-v0.01/refs/heads/master/img/' + keys[keysIndex ] + "Arrow.png", function(arrow) {
+        arrow.set({ 
+            left: 150,
+            top: 100,
+            selectable: false
+        });
+        arrow.scaleToWidth(150);
+        canvas.add(arrow);
+       });
+
+       fabric.Image.fromURL('https://raw.githubusercontent.com/Slopsk123/Slap_Hero-v0.01/refs/heads/master/img/' + keys[keysIndex +1] + "Arrow.png", function(arrow) {
+        arrow.set({ 
+            left: 100,
+            top: 100,
+            selectable: false
+        });
+        arrow.scaleToWidth(150);
+        canvas.add(arrow);
+       });
+
+       fabric.Image.fromURL('https://raw.githubusercontent.com/Slopsk123/Slap_Hero-v0.01/refs/heads/master/img/' + keys[keysIndex +2] + "Arrow.png", function(arrow) {
         arrow.set({ 
             left: 50,
             top: 100,
             selectable: false
         });
-        arrow.scaleToWidth(200)
+        arrow.scaleToWidth(150);
         canvas.add(arrow);
        });
+       
 }
 
 function BpmToMillis(){
